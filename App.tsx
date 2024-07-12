e7790b29335c307d8f565c944914ecb3824aca73
gkotsovilis <i.kotsovilis@gmail.com>
2024-07-12 17:44:23 +0300

    chore: configure gesture handler

--- App.tsx
+++ App.tsx
@@ -1,3 +1,9 @@
+/**
+ * This import should be at the top of the file. Make sure there's nothing else before it.
+ * Check out the installation steps here {@link https://reactnavigation.org/docs/stack-navigator/#installation}
+ */
+import 'react-native-gesture-handler';
+
 import { Providers } from '@/Providers';
 import { StatusBar } from 'expo-status-bar';
 import { Text } from 'react-native';
