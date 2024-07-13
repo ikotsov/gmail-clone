e6919314fd1e21ed34d04d7f7f7c54a8be37b66c
gkotsovilis <i.kotsovilis@gmail.com>
2024-07-13 16:50:24 +0300

    feat: render app navigator

--- App.tsx
+++ App.tsx
@@ -4,18 +4,15 @@
  */
 import 'react-native-gesture-handler';
 
+import AppNavigator from '@/nav/AppNavigator';
 import { Providers } from '@/Providers';
 import { StatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <Providers>
-      <SafeAreaView>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
+      <StatusBar style="auto" />
+      <AppNavigator />
     </Providers>
   );
 }
