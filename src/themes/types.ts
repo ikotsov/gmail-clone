cadf88da3b1a5eea0599d2fa933014b7f25db948
gkotsovilis <i.kotsovilis@gmail.com>
2024-07-14 17:45:50 +0300

    feat: add types

---
+++ src/themes/types.ts
@@ -0,0 +1,11 @@
+import darkTheme from './dark';
+import lightTheme from './light';
+
+export type AppThemes = {
+  light: typeof lightTheme;
+  dark: typeof darkTheme;
+};
+
+declare module 'react-native-unistyles' {
+  export interface UnistylesThemes extends AppThemes {}
+}
