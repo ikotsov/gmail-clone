53962a29efcf18a71b0f0675d073a19294d44bc4
gkotsovilis <i.kotsovilis@gmail.com>
2024-07-19 18:44:38 +0300

    feat: add theme type

--- src/themes/types.ts
+++ src/themes/types.ts
@@ -1,6 +1,8 @@
 import darkTheme from './dark';
 import lightTheme from './light';
 
+export type Theme = typeof lightTheme;
+
 export type AppThemes = {
   light: typeof lightTheme;
   dark: typeof darkTheme;
