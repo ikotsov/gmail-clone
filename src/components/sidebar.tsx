5365724571206fec9dc0586c163bde2a2771e5b9
gkotsovilis <i.kotsovilis@gmail.com>
2024-07-19 18:46:34 +0300

    style: sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,10 +1,20 @@
 import { DrawerContentComponentProps } from '@react-navigation/drawer';
-import { View } from 'react-native';
+import Box from './box';
+import Text from './text';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 type Props = DrawerContentComponentProps;
 
 function Sidebar(props: Props) {
-  return <View></View>;
+  return (
+    <Box fullScreen bg="sidebarBackground">
+      <SafeAreaView>
+        <Text color="sidebar" m="lg">
+          TEST
+        </Text>
+      </SafeAreaView>
+    </Box>
+  );
 }
 
 export default Sidebar;
